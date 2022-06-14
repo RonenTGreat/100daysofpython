20650fd24cf6058fe4aaf155c9f1acaa290e73f4
ronentgreat <ronenh53@gmail.com>
2022-06-14 22:06:36 +0000

    donw with day 7

--- Day7.py
+++ Day7.py
@@ -1,13 +1,43 @@
 import random
+from turtle import clear
+from hangman_art import logo, stages
+from hangman_words import word_list
 
-word_list = ["ardvark", "baboon", "camel"]
 
+print(logo)
+print("\n")
 chosen_word = random.choice(word_list)
+lives = 6
 
-guess = input("Guess a letter: ").lower()
+display = []
 
-for letter in chosen_word:
-  if guess == letter:
-    print("Right")
-  else:
-    print("Wrong")
+for dash in chosen_word:
+    display.append('_')
+print(display)
+
+end_of_game = False
+
+while not end_of_game:
+  guess = input("Guess a letter: ").lower()
+  print("\033[H\033[J")
+
+  if guess in display:
+    print(f"You've already guessed {guess}")
+
+  for position in range(len(chosen_word)):
+    letter = chosen_word[position]
+    if guess == letter:
+      display[position] = letter
+  if guess not in chosen_word:
+    print(f"You guessed {guess}, that's not in the word. You lose a life.")
+    lives -= 1
+    if lives == 0:
+      end_of_game = True
+      print("You lose.")
+    print(stages[lives])
+
+  print(f"{''.join(display)}")
+  if "_" not in display:      
+    end_of_game = True
+    print("You win.")
+  
