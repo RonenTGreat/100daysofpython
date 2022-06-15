1004dfedab6f60689beda596f44b26197ba9a1af
ronentgreat <ronenh53@gmail.com>
2022-06-15 20:04:02 +0000

    added decrypt function

--- Day8.py
+++ Day8.py
@@ -2,6 +2,7 @@ from turtle import position
 
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+            'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
             'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
@@ -18,4 +19,19 @@ def encrypt(plain_text, shift_amount):
     cipher_text += new_letter
   print(f"The encoded text is {cipher_text}")
 
-encrypt(text, shift)
+
+def decrypt(encrpt_text, shift_amount):
+  plain_text = ""
+  for letter in encrpt_text:
+    position = alphabet.index(letter)
+    new_position = position - shift_amount
+    new_letter = alphabet[new_position]
+    plain_text += new_letter
+  print(f"The decoded text is {plain_text}")
+
+if direction.lower() == "encode":
+  encrypt(plain_text=text,shift_amount=shift)
+elif direction.lower() == "decode": 
+  decrypt(encrpt_text=text, shift_amount=shift)
+else:
+  print("Please choose 'encode' or 'decode'")
