e28dabc6c7a12b5297159fc60a07e71def4f25f3
ronentgreat <ronenh53@gmail.com>
2022-06-15 20:41:01 +0000

    refracted functions

--- Day8.py
+++ Day8.py
@@ -1,6 +1,3 @@
-from turtle import position
-
-
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
             'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
             'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
@@ -10,28 +7,18 @@ text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 
-def encrypt(plain_text, shift_amount):
-  cipher_text = ""
-  for letter in plain_text:
+def caesar(cipher_direction, start_text, shift_amount):
+  end_text = ""
+  for letter in start_text:
     position = alphabet.index(letter)
-    new_position = position + shift_amount
+    if cipher_direction.lower() == 'encode':
+      new_position = position + shift_amount
+    elif cipher_direction.lower() == 'decode':
+      new_position = position - shift_amount
+    else:
+      print("Please ")
     new_letter = alphabet[new_position]
-    cipher_text += new_letter
-  print(f"The encoded text is {cipher_text}")
+    end_text += new_letter
+  print(f"The {cipher_direction}d text is {end_text}")
 
-
-def decrypt(encrpt_text, shift_amount):
-  plain_text = ""
-  for letter in encrpt_text:
-    position = alphabet.index(letter)
-    new_position = position - shift_amount
-    new_letter = alphabet[new_position]
-    plain_text += new_letter
-  print(f"The decoded text is {plain_text}")
-
-if direction.lower() == "encode":
-  encrypt(plain_text=text,shift_amount=shift)
-elif direction.lower() == "decode": 
-  decrypt(encrpt_text=text, shift_amount=shift)
-else:
-  print("Please choose 'encode' or 'decode'")
+caesar(cipher_direction=direction, start_text=text, shift_amount=shift)
