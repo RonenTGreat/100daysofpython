0c8d0f706326aa6108030b6397b8a529c1fa2dec
ronentgreat <ronenh53@gmail.com>
2022-06-16 21:59:35 +0000

    completed day 8

---
+++ Day 8/Day9.py
@@ -0,0 +1,35 @@
+from bisect import bisect
+from unicodedata import name
+from art import logo
+from os import system
+
+print(logo)
+print("Welcome to the secret auction program.")
+
+bids = {}
+
+def intro():
+  name = input("What is your name?: ")
+  bid = int(input("What's your bid? $"))
+  bids[name] = bid
+  
+
+
+intro()
+
+any_bidder = True
+while any_bidder:
+  any = input("Are there any other bidders? Type 'yes' or 'no'. \n")
+
+  if any == 'yes':
+    system('CLS')
+    intro()
+  else:
+    any_bidder = False
+
+higher = 0
+for name in bids:
+  if bids[name] > higher:
+    higher = bids[name]
+
+print(f"The winner is {name} witha bid of ${higher}.")
