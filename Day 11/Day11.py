69e08bd67fddd6d498e6d4a0ac167556b736ce48
ronentgreat <ronenh53@gmail.com>
2022-06-19 22:04:14 +0000

    done with hint 12

--- Day 11/Day11.py
+++ Day 11/Day11.py
@@ -43,6 +43,11 @@ while not is_game_over:
 
 #Hint 12: Once the user is done, it's time to let the computer play. The computer should keep drawing cards as long as it has a score less than 17.
 
+while computer_score != 0 and computer_score < 17:
+  computer_cards.append(deal_card(cards))
+  computer_score = calculate_score(computer_cards)
+
+
 #Hint 13: Create a function called compare() and pass in the user_score and computer_score. If the computer and user both have the same score, then it's a draw. If the computer has a blackjack (0), then the user loses. If the user has a blackjack (0), then the user wins. If the user_score is over 21, then the user loses. If the computer_score is over 21, then the computer loses. If none of the above, then the player with the highest score wins.
 
 #Hint 14: Ask the user if they want to restart the game. If they answer yes, clear the console and start a new game of blackjack and show the logo from art.py.
