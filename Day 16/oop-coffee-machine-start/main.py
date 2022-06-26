fb56bf85899f869cbbfb5e99cede907c888c19fd
ronentgreat <ronenh53@gmail.com>
2022-06-26 23:50:43 +0000

    continue tomorrow

--- Day 16/oop-coffee-machine-start/main.py
+++ Day 16/oop-coffee-machine-start/main.py
@@ -1,3 +1,20 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+
+is_running = True
+
+coffee_make = CoffeeMaker()
+money_report = MoneyMachine()
+menu = Menu()
+item = MenuItem()
+
+while is_running:
+  choice = input("What would you like? (espresso/latte/cappuccino): ").lower()
+
+  if choice == "off":
+    is_running = False
+  elif choice == "report":
+    coffee_make.report()
+    money_report.report()
