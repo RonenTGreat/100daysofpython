fcf8ab6e48d7d86336f34f764c473959401f9bee
ronentgreat <ronenh53@gmail.com>
2022-06-27 22:19:03 +0000

    changed folder name

--- Dau 17/main.py
+++
@@ -1,9 +0,0 @@
-from question_model import Question
-from data import question_data
-
-question_bank = []
-for question in question_data:
-  new_question = Question(question['text'],question['answer'])
-  question_bank.append(new_question)
-
-print(question_bank)
--- Dau 17/question_model.py
+++
@@ -1,4 +0,0 @@
-class Question:
-  def __init__(self, text, answer):
-    self.text = text
-    self.answer = answer
