b48e9e888a869d66c0ba09d56c3a2ee7849afb51
ronentgreat <ronenh53@gmail.com>
2022-06-30 23:25:09 +0000

    refractored

--- Day 18/hirst-painting-start/main.py
+++ Day 18/hirst-painting-start/main.py
@@ -9,24 +9,29 @@ color_list = [(202, 164, 110), (240, 245, 241), (236, 239, 243), (149, 75, 50), 
 turtle_module.colormode(255)
 tim = turtle_module.Turtle()
 
-tim.speed('fastest')
-tim.penup()
-tim.setheading(255)
-tim.forward(300)
-tim.setheading(0)
-numbers_of_dots = 100
+def reset_position():
+    tim.speed('fastest')
+    tim.penup()
+    tim.setheading(255)
+    tim.forward(300)
+    tim.setheading(0)
 
-for dot_counts in range(1, numbers_of_dots + 1):
-    tim.dot(20, random.choice(color_list))
-    tim.forward(50)
-
-    if dot_counts % 10 == 0:
-        tim.setheading(90)
+def paint():
+    reset_position()
+    numbers_of_dots = 100
+    for dot_counts in range(1, numbers_of_dots + 1):
+        tim.dot(20, random.choice(color_list))
         tim.forward(50)
-        tim.setheading(180)
-        tim.forward(500)
-        tim.setheading(0)
-tim.hideturtle()
+
+        if dot_counts % 10 == 0:
+            tim.setheading(90)
+            tim.forward(50)
+            tim.setheading(180)
+            tim.forward(500)
+            tim.setheading(0)
+    tim.hideturtle()
+
+paint()
 
 screen = turtle_module.Screen()
 screen.exitonclick()
