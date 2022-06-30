69ce2d04bcb22d4b22dc4b80ae64ffb9bb3e1621
ronentgreat <ronenh53@gmail.com>
2022-06-30 20:51:20 +0000

    done with part 1

--- Day 18/hirst-painting-start/main.py
+++ Day 18/hirst-painting-start/main.py
@@ -3,6 +3,9 @@ import colorgram
 rgb_colors = []
 colors = colorgram.extract('image.jpg', 30)
 for color in colors:
-    rgb_colors.append(color.rgb)
-
+    r = color.rgb.r
+    g = color.rgb.g
+    b = color.rgb.b
+    new_color = (r, g, b)
+    rgb_colors.append(new_color)
 print(rgb_colors)
