b523164eef0d201ea23cd819e4e6d9a836adfae2
ronentgreat <ronenh53@gmail.com>
2022-06-30 23:02:40 +0000

    project 18 done

--- Day 18/hirst-painting-start/main.py
+++ Day 18/hirst-painting-start/main.py
@@ -1,11 +1,32 @@
-import colorgram
+import turtle as turtle_module
+import random
 
-rgb_colors = []
-colors = colorgram.extract('image.jpg', 30)
-for color in colors:
-    r = color.rgb.r
-    g = color.rgb.g
-    b = color.rgb.b
-    new_color = (r, g, b)
-    rgb_colors.append(new_color)
-print(rgb_colors)
+color_list = [(202, 164, 110), (240, 245, 241), (236, 239, 243), (149, 75, 50), (222, 201, 136), (53, 93, 123),
+              (170, 154, 41), (138, 31, 20), (134, 163, 184), (197, 92, 73), (47, 121, 86), (73, 43, 35),
+              (145, 178, 149), (14, 98, 70), (232, 176, 165), (160, 142, 158), (54, 45, 50), (101, 75, 77),
+              (183, 205, 171), (36, 60, 74), (19, 86, 89), (82, 148, 129), (147, 17, 19), (27, 68, 102),
+              (12, 70, 64), (107, 127, 153), (176, 192, 208), (168, 99, 102)]
+turtle_module.colormode(255)
+tim = turtle_module.Turtle()
+
+tim.speed('fastest')
+tim.penup()
+tim.setheading(255)
+tim.forward(300)
+tim.setheading(0)
+numbers_of_dots = 100
+
+for dot_counts in range(1, numbers_of_dots + 1):
+    tim.dot(20, random.choice(color_list))
+    tim.forward(50)
+
+    if dot_counts % 10 == 0:
+        tim.setheading(90)
+        tim.forward(50)
+        tim.setheading(180)
+        tim.forward(500)
+        tim.setheading(0)
+tim.hideturtle()
+
+screen = turtle_module.Screen()
+screen.exitonclick()
