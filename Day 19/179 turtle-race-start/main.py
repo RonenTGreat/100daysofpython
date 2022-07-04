ba92e59d83d573ec8b3923c9a2317631625c4f82
ronentgreat <ronenh53@gmail.com>
2022-07-04 16:30:39 +0000

    completed

--- Day 19/179 turtle-race-start/main.py
+++ Day 19/179 turtle-race-start/main.py
@@ -22,9 +22,15 @@ if user_bet:
 
 while is_race_on:
     for turtle in all_turtle:
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You've won! The {winning_color} turtle is the winner!")
+            else:
+                print(f"You've lost! The {winning_color} turtle is the winner!")
         random_distance = random.randint(0, 10)
         turtle.forward(random_distance)
-        
 
 
 screen.exitonclick()
