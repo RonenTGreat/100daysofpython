4ca8979403c6fa306a32b514037ec3bb233d6b23
ronentgreat <ronenh53@gmail.com>
2022-07-04 12:40:00 +0000

    added multiple turle to screen

--- Day 19/179 turtle-race-start/main.py
+++ Day 19/179 turtle-race-start/main.py
@@ -1,7 +1,15 @@
 from turtle import Turtle, Screen
 
-tim = Turtle()
 screen = Screen()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a colour: ")
+colors = ["red", "orange", "green", "yellow", "blue", "purple"]
+y_position = [-70, -40, -10, 20, 50, 80]
 
+for turtle_index in range(0, 6):
+    tim = Turtle(shape="turtle")
+    tim.penup()
+    tim.color(colors[turtle_index])
+    tim.goto(x=-230, y=y_position[turtle_index])
 
 screen.exitonclick()
