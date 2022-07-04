5191403e58e28f5df33998e0711ac642de5d1350
ronentgreat <ronenh53@gmail.com>
2022-07-04 16:20:25 +0000

    turtles are able to move now

--- Day 19/179 turtle-race-start/main.py
+++ Day 19/179 turtle-race-start/main.py
@@ -1,15 +1,30 @@
+import random
+import turtle
 from turtle import Turtle, Screen
 
+is_race_on = False
 screen = Screen()
 screen.setup(width=500, height=400)
 user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a colour: ")
 colors = ["red", "orange", "green", "yellow", "blue", "purple"]
 y_position = [-70, -40, -10, 20, 50, 80]
+all_turtle = []
 
 for turtle_index in range(0, 6):
-    tim = Turtle(shape="turtle")
-    tim.penup()
-    tim.color(colors[turtle_index])
-    tim.goto(x=-230, y=y_position[turtle_index])
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.penup()
+    new_turtle.color(colors[turtle_index])
+    new_turtle.goto(x=-230, y=y_position[turtle_index])
+    all_turtle.append(new_turtle)
+
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+    for turtle in all_turtle:
+        random_distance = random.randint(0, 10)
+        turtle.forward(random_distance)
+        
+
 
 screen.exitonclick()
