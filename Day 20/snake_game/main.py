fdecf654b35bef21d5a722f8df5bb65db36304fc
ronentgreat <ronenh53@gmail.com>
2022-07-05 20:54:34 +0000

    setup file

---
+++ Day 20/snake_game/main.py
@@ -0,0 +1,18 @@
+from turtle import Screen, Turtle
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
