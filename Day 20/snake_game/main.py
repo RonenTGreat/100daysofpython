9ef4e342b336053287a4acf9bce5eaee3b1cbf79
ronentgreat <ronenh53@gmail.com>
2022-07-05 21:51:17 +0000

    completed the movement

--- Day 20/snake_game/main.py
+++ Day 20/snake_game/main.py
@@ -1,9 +1,11 @@
 from turtle import Screen, Turtle
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
+screen.tracer(0)
 
 starting_position = [(0, 0), (-20, 0), (-40, 0)]
 squares = []
@@ -15,11 +17,18 @@ for position in starting_position:
     square.goto(position)
     squares.append(square)
 
+
 game_is_on = True
 
 while game_is_on:
-    for element in squares:
-        element.forward(1)
+    screen.update()
+    time.sleep(0.1)
+
+    for square_num in range(len(squares) - 1, 0, -1):
+        new_x = squares[square_num - 1].xcor()
+        new_y = squares[square_num - 1].ycor()
+        squares[square_num].goto(new_x, new_y)
+    squares[0].forward(20)
 
 
 
