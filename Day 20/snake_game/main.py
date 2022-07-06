eaca499a6976fd2b803e7ab32c7f405cff16c8ef
ronentgreat <ronenh53@gmail.com>
2022-07-06 00:01:58 +0000

    added controls

--- Day 20/snake_game/main.py
+++ Day 20/snake_game/main.py
@@ -13,6 +13,13 @@ game_is_on = True
 
 snake = Snake()
 
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+
 while game_is_on:
     screen.update()
     time.sleep(0.1)
