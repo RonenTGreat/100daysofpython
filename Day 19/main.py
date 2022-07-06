a7d058e1a0f4d8224b19be6ff34b7fdce2acbb70
ronentgreat <ronenh53@gmail.com>
2022-07-06 19:49:25 +0000

    inheritance

--- Day 19/main.py
+++ Day 19/main.py
@@ -1,14 +1,41 @@
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
+#
+# tim = Turtle()
+# screen = Screen()
+#
+#
+# def move_forwards():
+#     tim.forward(10)
+#
+#
+# screen.listen()
+#
+# screen.onkey(key="space", fun=move_forwards)
+# screen.exitonclick()
 
-tim = Turtle()
-screen = Screen()
+
+class Animal:
+    def __init__(self):
+        self.num_eyes = 2
+
+    def breathe(self):
+        print("Inhale, exhale.")
 
 
-def move_forwards():
-    tim.forward(10)
+class Fish(Animal):
+    def __init__(self):
+        super().__init__()
+
+    def breathe(self):
+        super().breathe()
+        print("doing this underwater.")
+
+    def swim(self):
+        print("moving in water.")
 
 
-screen.listen()
+nemo = Fish()
 
-screen.onkey(key="space", fun=move_forwards)
-screen.exitonclick()
+nemo.swim()
+nemo.breathe()
+print(nemo.num_eyes)
