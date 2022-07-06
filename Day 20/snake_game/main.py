70120e78fcdc82176cbb0ab4137788b4a04b754b
ronentgreat <ronenh53@gmail.com>
2022-07-06 21:39:07 +0000

    added ability for sanke to grow

--- Day 20/snake_game/main.py
+++ Day 20/snake_game/main.py
@@ -32,6 +32,7 @@ while game_is_on:
     # Detects collision with food.
     if snake.head.distance(food) < 15:
         food.refresh()
+        snake.grow()
         score.clear()
         score.increase()
 
@@ -39,6 +40,6 @@ while game_is_on:
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
         game_is_on = False
         score.game_over()
-
+    
 
 screen.exitonclick()
--- Day 20/snake_game/snake.py
+++ Day 20/snake_game/snake.py
@@ -16,11 +16,17 @@ class Snake:
 
     def create_snake(self):
         for position in STARTING_POSITION:
-            new_segment = Turtle("square")
-            new_segment.color("white")
-            new_segment.penup()
-            new_segment.goto(position)
-            self.segments.append(new_segment)
+            self.add_segment(position)
+
+    def add_segment(self, position):
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def grow(self):
+        self.add_segment(self.segments[-1].position())
 
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
