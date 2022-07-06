e121d9f81a10ba21e48a4294605eb1d1bf4db6a7
ronentgreat <ronenh53@gmail.com>
2022-07-06 21:52:03 +0000

    added gameover if collision to tail

--- Day 20/snake_game/main.py
+++ Day 20/snake_game/main.py
@@ -40,6 +40,14 @@ while game_is_on:
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
         game_is_on = False
         score.game_over()
-    
+
+    # Detect collision with tail
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+            score.game_over()
+
 
 screen.exitonclick()
