a75405616e7d3bb4a9b45e46deb27612f44114ad
ronentgreat <ronenh53@gmail.com>
2022-07-07 15:37:35 +0000

    added paddle

--- Game_Pong/main.py
+++ Game_Pong/main.py
@@ -1,9 +1,30 @@
 from turtle import Screen
+from paddle import Paddle
+
 
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.title("Ping Pong")
 screen.bgcolor("black")
+screen.tracer(0)
+
+paddle_1 = Paddle((350, 0))
+paddle_2 = Paddle((-350, 0))
+
+
+screen.listen()
+
+
+screen.onkey(paddle_1.go_up, "Up")
+screen.onkey(paddle_1.go_down, "Down")
+screen.onkey(paddle_2.go_up, "w")
+screen.onkey(paddle_2.go_down, "s")
+
+
+game_is_on = True
+
+while game_is_on:
+    screen.update()
 
 
 screen.exitonclick()
