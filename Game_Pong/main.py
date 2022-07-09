fbcfa469ef8a546e62b99aece9b025db183f575e
ronentgreat <ronenh53@gmail.com>
2022-07-09 14:19:08 +0000

    added scoreboard

--- Game_Pong/main.py
+++ Game_Pong/main.py
@@ -25,7 +25,7 @@ screen.onkey(paddle_2.go_down, "s")
 game_is_on = True
 
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
 
@@ -33,9 +33,9 @@ while game_is_on:
     if ball.ycor() > 280 or ball.ycor() < -280:
         ball.bounce_y()
 
-    # Detect collision with paddle 1
+    # Detect collision with paddle
 
-    if ball.distance(paddle_1) < 50 and ball.xcor() > 320 or ball.distance(paddle_2) < 50 and ball.xcor() > - 320:
+    if ball.distance(paddle_1) < 50 and ball.xcor() > 320 or ball.distance(paddle_2) < 50 and ball.xcor() < - 320:
         ball.bounce_x()
 
     # Ball goes out of bound by right paddle
