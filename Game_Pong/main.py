ec74e6ea330af04012076a4ce5ee2a25e7793e0e
ronentgreat <ronenh53@gmail.com>
2022-07-09 12:47:16 +0000

    added bounce when ball hits paddle

--- Game_Pong/main.py
+++ Game_Pong/main.py
@@ -3,7 +3,6 @@ from paddle import Paddle
 from ball import Ball
 import time
 
-
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.title("Ping Pong")
@@ -16,12 +15,10 @@ ball = Ball()
 
 screen.listen()
 
-
 screen.onkey(paddle_1.go_up, "Up")
 screen.onkey(paddle_1.go_down, "Down")
 screen.onkey(paddle_2.go_up, "w")
 screen.onkey(paddle_2.go_down, "s")
-
 
 game_is_on = True
 
@@ -30,7 +27,14 @@ while game_is_on:
     screen.update()
     ball.move()
 
+    # Detect collision with wall
     if ball.ycor() > 280 or ball.ycor() < -280:
-        ball.bounce()
+        ball.bounce_y()
+
+    # Detect collision with paddle 1
+
+    if ball.distance(paddle_1) < 50 and ball.xcor() > 320 or ball.distance(paddle_2) < 50 and ball.xcor() > - 320:
+        ball.bounce_x()
+
 
 screen.exitonclick()
