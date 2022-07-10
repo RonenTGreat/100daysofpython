0a660ee5f3bebf0a28d5ff336ffc7e5197414206
ronentgreat <ronenh53@gmail.com>
2022-07-10 23:24:17 +0000

    generated cars

--- turtle-crossing/main.py
+++ turtle-crossing/main.py
@@ -9,6 +9,7 @@ screen.setup(width=600, height=600)
 screen.tracer(0)
 
 player = Player()
+cars = CarManager()
 
 screen.listen()
 screen.onkey(player.move_up, "Up")
@@ -17,3 +18,5 @@ game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    cars.create_car()
+    cars.move_cars()
