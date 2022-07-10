fa1eabdad79a2e391d5460f61d4a32289cfcb43f
ronentgreat <ronenh53@gmail.com>
2022-07-10 22:40:01 +0000

    added turtle to screen

--- turtle-crossing/main.py
+++ turtle-crossing/main.py
@@ -8,6 +8,8 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+player = Player()
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
--- turtle-crossing/player.py
+++ turtle-crossing/player.py
@@ -3,5 +3,14 @@ MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+from turtle import Turtle
+
+
+class Player(Turtle):
+    def __init__(self):
+        super(Player, self).__init__()
+        self.shape("turtle")
+        self.penup()
+        self.setheading(90)
+        self.goto(0, -280)
+
