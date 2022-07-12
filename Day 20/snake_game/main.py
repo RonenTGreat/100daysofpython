bb159d2669d724d6ade186ded0411d60e3eb3aaf
ronentgreat <ronenh53@gmail.com>
2022-07-12 20:59:51 +0000

    removed gameover feature

--- Day 20/snake_game/main.py
+++ Day 20/snake_game/main.py
@@ -38,14 +38,14 @@ while game_is_on:
 
     # Detect collision with wall.
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        score.game_over()
+        score.reset()
+        snake.reset()
 
     # Detect collision with tail
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            score.game_over()
+            score.reset()
+            snake.reset()
 
 
 screen.exitonclick()
--- Day 20/snake_game/snake.py
+++ Day 20/snake_game/snake.py
@@ -25,6 +25,13 @@ class Snake:
         new_segment.goto(position)
         self.segments.append(new_segment)
 
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000, 1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
+
     def grow(self):
         self.add_segment(self.segments[-1].position())
 
@@ -50,4 +57,3 @@ class Snake:
     def right(self):
         if self.head.heading() != LEFT:
             self.head.setheading(RIGHT)
-
