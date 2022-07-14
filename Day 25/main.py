ba39a5e3d9bc3437a59cb7d740aaecfa295e0865
ronentgreat <ronenh53@gmail.com>
2022-07-14 21:32:15 +0000

    .

--- Day 25/main.py
+++ Day 25/main.py
@@ -14,5 +14,37 @@
 
 import pandas
 data = pandas.read_csv("weather_data.csv")
-print(data["temp"])
+# print(data["temp"])
 
+# data_dict = data.to_dict()
+# print(data_dict)
+#
+# temp_list = data['temp'].to_list()
+# print(temp_list)
+#
+# print(data['temp'].mean())
+# print(data['temp'].max())
+#
+# # Get Data in Columns
+# print(data['condition'])
+# print(data.condition)
+
+# Get Data in Row
+# print(data[data.day == "Monday"])
+# print(data[data.temp == data.temp.max()])
+
+# monday = data[data.day == 'Monday']
+# # print(monday.condition)
+#
+# cel_to_feh = (int(monday.temp) * (9/5) + 32)
+# print(cel_to_feh)
+
+# Create a dataframe from scratch
+
+data_dict = {
+    "students": ["Amy", "James", "Angela"],
+    "scores": [76, 56, 65]
+}
+
+data = pandas.DataFrame(data_dict)
+data.to_csv("new_date.csv ")
