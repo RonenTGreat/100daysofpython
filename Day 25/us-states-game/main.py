2c4e0da1373bc54db21f549d9ad313ff16ef3708
ronentgreat <ronenh53@gmail.com>
2022-07-15 23:00:27 +0000

    .

--- Day 25/us-states-game/main.py
+++ Day 25/us-states-game/main.py
@@ -11,20 +11,21 @@ location = turtle.Turtle()
 location.hideturtle()
 location.penup()
 
-answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
-answer_state.title()
 
 data = pandas.read_csv("50_states.csv")
 
 state = data["state"].to_list()
 
-if answer_state.title() in state:
-    x_cor = int(data[data.state == answer_state.title()].x)
-    y_cor = int(data[data.state == answer_state.title()].y)
-    location.goto(x_cor, y_cor)
-    location.write(f"{answer_state}")
+correct_guess = []
 
 
-
+while len(correct_guess) < 50:
+    answer_state = screen.textinput(title=f"{len(correct_guess)}/50 Guess the State", prompt="What's another state's name?").title()
+    if answer_state in state:
+        correct_guess.append(answer_state)
+        x_cor = int(data[data.state == answer_state.title()].x)
+        y_cor = int(data[data.state == answer_state.title()].y)
+        location.goto(x_cor, y_cor)
+        location.write(f"{answer_state}")
 
 screen.exitonclick()
