7168f9986d94986393d24114635d8ea43c4a0d0f
ronentgreat <ronenh53@gmail.com>
2022-07-18 12:47:42 +0000

    added label to window

--- Day 27/main.py
+++ Day 27/main.py
@@ -5,6 +5,10 @@ window.title("My First GUI Program")
 window.minsize(width=500, height=400)
 
 
+# Label
+my_label = tkinter.Label(text="I am a label", font=("Arial", 24, "bold"))
+my_label.pack(side="left")
+
 
 
 
