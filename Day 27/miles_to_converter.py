ce701e6eb3ea261d6929a7ebc1601813f74cdd4d
ronentgreat <ronenh53@gmail.com>
2022-07-19 22:24:02 +0000

    finished

--- Day 27/miles_to_converter.py
+++ Day 27/miles_to_converter.py
@@ -1,15 +1,17 @@
 from tkinter import *
-value = 0
 
 window = Tk()
 window.title("Mile to Km Converter")
-window.minsize(width=200, height=100)
+window.minsize(width=285, height=100)
+window.config(padx=20, pady=20)
+
 
 def convert():
-    value = int(entry.get()) * 1.609
-    conversion_value.config(text=f"{value}")
+    value = float(entry.get()) * 1.609
+    conversion_value.config(text=f"{value.__round__(2)}")
 
-entry = Entry(width=20)
+
+entry = Entry(width=7)
 entry.grid(column=1, row=1)
 
 unit = Label(text="Miles")
@@ -18,7 +20,7 @@ unit.grid(column=3, row=1)
 label_1 = Label(text="is equal to")
 label_1.grid(column=0, row=2)
 
-conversion_value = Label(text=f"{value}")
+conversion_value = Label(text=f"{0}")
 conversion_value.grid(column=1, row=2)
 
 value_unit = Label(text="Km")
@@ -26,13 +28,5 @@ value_unit.grid(column=2, row=2)
 
 button = Button(text="Calculate", command=convert)
 button.grid(column=1, row=3)
-
-
-
-
-
-
-
-
 
 window.mainloop()
