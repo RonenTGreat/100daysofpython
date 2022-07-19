e6e45c087e321260d8b057cd8bbec644d38da46b
ronentgreat <ronenh53@gmail.com>
2022-07-19 21:51:46 +0000

    added conversion text

--- Day 27/miles_to_converter.py
+++ Day 27/miles_to_converter.py
@@ -10,6 +10,18 @@ entry.grid(column=0, row=1)
 unit = Label(text="Miles")
 unit.grid(column=1, row=1)
 
+label_1 = Label(text="is equal to")
+label_1.grid(column=0, row=2)
+
+value = 0
+conversion_value = Label(text=f"{value}")
+conversion_value.grid(column=1, row=2)
+
+value_unit = Label(text="Km")
+value_unit.grid(column=2, row=2)
+
+
+
 
 
 
