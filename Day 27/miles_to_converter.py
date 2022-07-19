d00ade57e085d056a29c2f4584be273897d5a16b
ronentgreat <ronenh53@gmail.com>
2022-07-19 21:46:38 +0000

    added label and text field

---
+++ Day 27/miles_to_converter.py
@@ -0,0 +1,20 @@
+from tkinter import *
+window = Tk()
+window.title("Mile to Km Converter")
+window.minsize(width=300, height=200)
+
+
+entry = Entry(width=20)
+entry.grid(column=0, row=1)
+
+unit = Label(text="Miles")
+unit.grid(column=1, row=1)
+
+
+
+
+
+
+
+
+window.mainloop()
