382e9ed2779426d5dcb3d1f6c1c7e0473a882891
ronentgreat <ronenh53@gmail.com>
2022-07-20 15:20:24 +0000

    added coordinates for map

--- Know_Ghana/main.py
+++ Know_Ghana/main.py
@@ -6,6 +6,10 @@ image = "blank_ghana_regional_map.gif"
 screen.addshape(image)
 turtle.shape(image)
 
+def get_mouse_click_coor(x, y):
+    print(x, y)
+
+turtle.onscreenclick(get_mouse_click_coor)
 
 
 screen.mainloop()
