c0e57f80a508e5ca4163abeab3f37a9f477ef221
ronentgreat <ronenh53@gmail.com>
2022-07-20 19:54:46 +0000

    added heading

--- pomodoro/main.py
+++ pomodoro/main.py
@@ -25,7 +25,10 @@ canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(column=1, row=2)
+
+heading = Label(text="Timer", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 30, "bold"))
+heading.grid(column=1, row=0)
 
 
 
