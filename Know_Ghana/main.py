4a75a3653b66b90efeaabe52a8c2a0ca3507cba1
ronentgreat <ronenh53@gmail.com>
2022-07-20 14:47:24 +0000

    inital commit

---
+++ Know_Ghana/main.py
@@ -0,0 +1,11 @@
+import turtle
+
+screen = turtle.Screen()
+screen.title("Ghana Region Game")
+image = "blank_ghana_regional_map.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+
+
+screen.mainloop()
