34ba1ce6cde6a734c671b5a4cd36fe337c655369
ronentgreat <ronenh53@gmail.com>
2022-07-20 15:38:04 +0000

    Completed

--- Know_Ghana/main.py
+++ Know_Ghana/main.py
@@ -1,15 +1,31 @@
 import turtle
-
+import pandas
 screen = turtle.Screen()
 screen.title("Ghana Region Game")
 image = "blank_ghana_regional_map.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-def get_mouse_click_coor(x, y):
-    print(x, y)
+location = turtle.Turtle()
+location.hideturtle()
+location.penup()
 
-turtle.onscreenclick(get_mouse_click_coor)
+data = pandas.read_csv("16_regions.csv")
+regions = data["region"].to_list()
 
+correct_guess = []
 
-screen.mainloop()
+while len(correct_guess) < 16:
+    answer = screen.textinput(title=f"{len(correct_guess)}/16 Guess the Region",
+                              prompt="What's another region's name").title()
+
+    if answer == "Exit":
+        missing_regions = [region for region in regions if region not in correct_guess]
+        learn = pandas.DataFrame(missing_regions)
+        learn.to_csv("regions_to_learn.csv")
+        break
+    if answer in regions:
+        correct_guess.append(answer)
+        location.goto(int(data[data.region == answer.title()].x), int(data[data.region == answer.title()].y))
+        location.write(answer)
+
