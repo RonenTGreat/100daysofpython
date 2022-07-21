4305780e181e4ec394462fa60991dd93c7886101
ronentgreat <ronenh53@gmail.com>
2022-07-21 23:53:46 +0000

    added reset functionality

--- pomodoro/main.py
+++ pomodoro/main.py
@@ -10,15 +10,21 @@ WORK_MIN = 1
 SHORT_BREAK_MIN = 1
 LONG_BREAK_MIN = 20
 reps = 0
+timer = None
+
 
 # ---------------------------- TIMER RESET ------------------------------- # 
-
+def reset_timer():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    check.config(text="")
+    heading.config(text="Timer")
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+
 
 def start_timer():
     global reps
     reps += 1
-
     work_sec = WORK_MIN * 60
     short_break_sec = SHORT_BREAK_MIN * 60
     long_break_sec = LONG_BREAK_MIN * 60
@@ -44,10 +50,10 @@ def count_down(count):
     if count_sec < 10:
         count_sec = f"0{count_sec}"
 
-
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
-        window.after(1000, count_down, count - 1)
+        global timer
+        timer = window.after(1000, count_down, count - 1)
     else:
         start_timer()
         marks = ""
@@ -56,9 +62,8 @@ def count_down(count):
             marks += "✔"
         check.config(text=marks)
 
-
-
 # ---------------------------- UI SETUP ------------------------------- #
+
 
 window = Tk()
 window.title("Pomodoro")
@@ -71,7 +76,6 @@ timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT
 canvas.grid(column=1, row=2)
 
 
-
 # Heading(Timer)
 heading = Label(text="Timer", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 50, "bold"))
 heading.grid(column=1, row=0)
@@ -81,7 +85,7 @@ start_button = Button(text="Start", command=start_timer, highlightthickness=0)
 start_button.grid(column=0, row=3)
 
 # Reset Button
-reset_button = Button(text="Reset", highlightthickness=0)
+reset_button = Button(text="Reset", command=reset_timer, highlightthickness=0)
 reset_button.grid(column=2, row=3)
 
 # Check mark
