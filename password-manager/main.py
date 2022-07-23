464bcfeb7a7f7e645bfd44a2e872505dea7e5ca6
ronentgreat <ronenh53@gmail.com>
2022-07-23 22:33:40 +0000

    added website label and input

--- password-manager/main.py
+++ password-manager/main.py
@@ -11,7 +11,15 @@ window.config(padx=20, pady=20)
 canvas = Canvas(width=200, height=200)
 logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img)
-canvas.grid(column=1, row=2)
+canvas.grid(column=1, row=0)
+
+# Website input
+website_label = Label(text="Website:")
+website_label.grid(column=0, row=1)
+website_input = Entry(width=35)
+website_input.grid(column=1, row=1, columnspan=2)
+
+
 
 
 window.mainloop()
