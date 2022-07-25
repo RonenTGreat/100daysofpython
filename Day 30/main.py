0a932a46069ca4a44655181bc2a7892517004680
ronentgreat <ronenh53@gmail.com>
2022-07-25 21:51:23 +0000

    raise error

--- Day 30/main.py
+++ Day 30/main.py
@@ -1,18 +1,26 @@
 # FileNotFound
+#
+# try:
+#     file = open("a_file.txt")
+#     a_dictinary = {"key": "value"}
+#     value = a_dictinary["key"]
+#     print(value)
+# except FileNotFoundError:
+#     file = open("a_file.txt", "w")
+#     file.write("Something")
+# except KeyError as error_message:
+#     print(f"The key {error_message} does not exist.")
+# else:
+#     content = file.read()
+#     print(content)
+# finally:
+#     raise TypeError("This is an error that I made up.")
 
-try:
-    file = open("a_file.txt")
-    a_dictinary = {"key": "value"}
-    value = a_dictinary["key"]
-    print(value)
-except FileNotFoundError:
-    file = open("a_file.txt", "w")
-    file.write("Something")
-except KeyError as error_message:
-    print(f"The key {error_message} does not exist.")
-else:
-    content = file.read()
-    print(content)
-finally:
-    file.close()
-    print("File was closed.")
+height = float(input("Height: "))
+weight = int(input("Weight: "))
+
+if height / 3:
+    raise ValueError("Human Height should not be over 3 meters.")
+
+bmi = weight / height ** 2
+
