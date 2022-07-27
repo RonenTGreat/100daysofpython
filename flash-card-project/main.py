6754aa9069d670587c9db2d54e43356846d3b5b4
ronentgreat <ronenh53@gmail.com>
2022-07-27 23:19:00 +0000

    added canvas

--- flash-card-project/main.py
+++ flash-card-project/main.py
@@ -1,9 +1,20 @@
 from tkinter import *
 BACKGROUND_COLOR = "#B1DDC6"
+FONT_NAME = "Ariel"
 
+
+#
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
+card_front_img = PhotoImage(file="./images/card_front.png")
+canvas.create_image(400, 263, image=card_front_img)
+title_text = canvas.create_text(400, 150, text="Title", fill="black", font=(FONT_NAME, 40, "italic"))
+word_text = canvas.create_text(400, 263, text="Word", fill="black", font=(FONT_NAME, 60, "bold"))
+canvas.grid(column=0, row=0, columnspan=2)
+
 
 
 
