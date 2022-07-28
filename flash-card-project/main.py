ad559fb006a809abfec24c7644b0d9fa9ea1a2ff
ronentgreat <ronenh53@gmail.com>
2022-07-28 20:07:37 +0000

    added function to generate french words

--- flash-card-project/main.py
+++ flash-card-project/main.py
@@ -1,9 +1,23 @@
 from tkinter import *
+import pandas
+import random
 BACKGROUND_COLOR = "#B1DDC6"
 FONT_NAME = "Ariel"
 
 
-#
+# ----------------------------------------GENERATE WORD-------------------------------------
+def generate_word():
+    data = pandas.read_csv("data/french_words.csv")
+    words = data.to_dict(orient="records")
+    current_card = random.choice(words)
+    word = current_card["French"]
+
+    canvas.itemconfig(word_text, text=word)
+    canvas.itemconfig(title_text, text="French")
+
+
+
+# ----------------------------------------UI SETUP-------------------------------------
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
@@ -11,19 +25,20 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 card_front_img = PhotoImage(file="./images/card_front.png")
 canvas.create_image(400, 263, image=card_front_img)
-title_text = canvas.create_text(400, 150, text="Title", fill="black", font=(FONT_NAME, 40, "italic"))
-word_text = canvas.create_text(400, 263, text="Word", fill="black", font=(FONT_NAME, 60, "bold"))
+title_text = canvas.create_text(400, 150, text="", fill="black", font=(FONT_NAME, 40, "italic"))
+word_text = canvas.create_text(400, 263, text="", fill="black", font=(FONT_NAME, 60, "bold"))
 canvas.grid(column=0, row=0, columnspan=2)
 
 # Button Wrong
 wrong_img = PhotoImage(file="./images/wrong.png")
-wrong_button = Button(image=wrong_img, highlightthickness=0)
+wrong_button = Button(image=wrong_img, command=generate_word, highlightthickness=0)
 wrong_button.grid(column=0, row=1)
 
 # Button Right
 right_img = PhotoImage(file="./images/right.png")
-right_button = Button(image=right_img, highlightthickness=0)
+right_button = Button(image=right_img, command=generate_word, highlightthickness=0)
 right_button.grid(column=1, row=1)
+generate_word()
 
 
 
