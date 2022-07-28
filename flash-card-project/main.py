a24f72d7060be479795a170bf9cf022f11b6ae64
ronentgreat <ronenh53@gmail.com>
2022-07-28 20:25:31 +0000

    added back for english translation part

--- flash-card-project/main.py
+++ flash-card-project/main.py
@@ -14,14 +14,15 @@ def generate_word():
     global current_card
     current_card = random.choice(words)
     word = current_card["French"]
-
     canvas.itemconfig(word_text, text=word)
     canvas.itemconfig(title_text, text="French")
 
 # ----------------------------------------FLIP CARD-------------------------------------
 def flip_card():
-    canvas.itemconfig(title_text, text="English")
-    canvas.itemconfig(word_text, text=current_card["English"])
+    canvas.itemconfig(title_text, text="English", fill="white")
+    canvas.itemconfig(word_text, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_background, image=card_back_img)
+
 
 
 
@@ -33,8 +34,9 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 window.after(3000, func=flip_card)
 
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
+card_back_img = PhotoImage(file="./images/card_back.png")
 card_front_img = PhotoImage(file="./images/card_front.png")
-canvas.create_image(400, 263, image=card_front_img)
+card_background = canvas.create_image(400, 263, image=card_front_img)
 title_text = canvas.create_text(400, 150, text="", fill="black", font=(FONT_NAME, 40, "italic"))
 word_text = canvas.create_text(400, 263, text="", fill="black", font=(FONT_NAME, 60, "bold"))
 canvas.grid(column=0, row=0, columnspan=2)
