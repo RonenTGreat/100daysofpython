e45e6f069cec439af0fd56933691ab242c31083c
ronentgreat <ronenh53@gmail.com>
2022-07-31 17:59:27 +0000

    finished

--- Day-32/birthday-wisher/main.py
+++ Day-32/birthday-wisher/main.py
@@ -1,32 +1,34 @@
 import pandas
+import datetime as dt
+import random
+import smtplib
 
-today = (7, 31)
+now = dt.datetime.now()
+today = (now.month, now.day)
 
-# HINT 2: Use pandas to read the birthdays.csv
 data = pandas.read_csv("./birthdays.csv")
-print(data)
+birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
+# print(birthdays_dict)
 
-# HINT 3: Use dictionary comprehension to create a dictionary from birthday.csv that is formated like this:
-# birthdays_dict = {
-#     (birthday_month, birthday_day): data_row
-# }
-#Dictionary comprehension template for pandas DataFrame looks like this:
-# new_dict = {new_key: new_value for (index, data_row) in data.iterrows()}
-#e.g. if the birthdays.csv looked like this:
-# name,email,year,month,day
-# Angela,angela@email.com,1995,12,24
-#Then the birthdays_dict should look like this:
-# birthdays_dict = {
-#     (12, 24): Angela,angela@email.com,1995,12,24
-# }
 
 #HINT 4: Then you could compare and see if today's month/day tuple matches one of the keys in birthday_dict like this:
-# if (today_month, today_day) in birthdays_dict:
+if today in birthdays_dict:
+    random_number = random.randint(1, 3)
+    birthday_person = birthdays_dict[today]
+    file_path = f"./letter_templates/letter_{random_number}.txt"
+    with open(file_path) as letter:
+        contents = letter.read()
+        contents = contents.replace("[NAME]", birthday_person["name"])
 
-# 3. If there is a match, pick a random letter (letter_1.txt/letter_2.txt/letter_3.txt) from letter_templates and replace the [NAME] with the person's actual name from birthdays.csv
-# HINT 1: Think about the relative file path to open each letter. 
-# HINT 2: Use the random module to get a number between 1-3 to pick a randome letter.
-# HINT 3: Use the replace() method to replace [NAME] with the actual name. https://www.w3schools.com/python/ref_string_replace.asp
+    my_email = "ronenh53@gmail.com"
+    my_password = "werilnphzbagjsjt"
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=my_password)
+        connection.sendmail(from_addr=my_email,
+                            to_addrs=birthday_person['email'],
+                            msg=f"Subject: Happy Birthday \n\n{contents}")
 
 # 4. Send the letter generated in step 3 to that person's email address.
 # HINT 1: Gmail(smtp.gmail.com), Yahoo(smtp.mail.yahoo.com), Hotmail(smtp.live.com), Outlook(smtp-mail.outlook.com)
