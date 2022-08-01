893eb2d2d9fe49e1df72a15daa3aa1027f24be3e
ronentgreat <ronenh53@gmail.com>
2022-08-01 20:20:14 +0000

    done

--- Day 33/kanye-quotes/main.py
+++ Day 33/kanye-quotes/main.py
@@ -1,10 +1,13 @@
 from tkinter import *
+import requests
 
 
 def get_quote():
-    pass
-    #Write your code here.
-
+    response = requests.get(url="https://api.kanye.rest")
+    response.raise_for_status()
+    data = response.json()
+    quote = data['quote']
+    canvas.itemconfig(quote_text, text=quote)
 
 
 window = Tk()
@@ -14,7 +17,7 @@ window.config(padx=50, pady=50)
 canvas = Canvas(width=300, height=414)
 background_img = PhotoImage(file="background.png")
 canvas.create_image(150, 207, image=background_img)
-quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes HERE", width=250, font=("Arial", 30, "bold"), fill="white")
+quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes HERE", width=250, font=("Arial", 25, "bold"), fill="white")
 canvas.grid(row=0, column=0)
 
 kanye_img = PhotoImage(file="kanye.png")
