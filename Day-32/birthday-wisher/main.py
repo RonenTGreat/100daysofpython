c468010e99cddb9fb2880c3d21eb8d259d6e3a96
ronentgreat <ronenh53@gmail.com>
2022-08-02 20:54:36 +0000

    removed comments

--- Day-32/birthday-wisher/main.py
+++ Day-32/birthday-wisher/main.py
@@ -12,10 +12,7 @@ today = (now.month, now.day)
 
 data = pandas.read_csv("./birthdays.csv")
 birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
-# print(birthdays_dict)
 
-
-#HINT 4: Then you could compare and see if today's month/day tuple matches one of the keys in birthday_dict like this:
 if today in birthdays_dict:
     random_number = random.randint(1, 3)
     birthday_person = birthdays_dict[today]
@@ -34,11 +31,6 @@ if today in birthdays_dict:
                             to_addrs=birthday_person['email'],
                             msg=f"Subject: Happy Birthday \n\n{contents}")
 
-# 4. Send the letter generated in step 3 to that person's email address.
-# HINT 1: Gmail(smtp.gmail.com), Yahoo(smtp.mail.yahoo.com), Hotmail(smtp.live.com), Outlook(smtp-mail.outlook.com)
-# HINT 2: Remember to call .starttls()
-# HINT 3: Remember to login to your email service with email/password. Make sure your security setting is set to allow less secure apps.
-# HINT 4: The message should have the Subject: Happy Birthday then after \n\n The Message Body.
 
 
 
