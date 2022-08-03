5f5de55dc473e5568c738f9e76b3682ccea911cc
ronentgreat <ronenh53@gmail.com>
2022-08-03 22:53:20 +0000

    ui setup

--- quizzler-app/ui.py
+++ quizzler-app/ui.py
@@ -1,2 +1,9 @@
+from tkinter import *
 THEME_COLOR = "#375362"
 
+class QuizInterface:
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quizxler")
+
+        self.window.mainloop()
