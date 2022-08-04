36a8b5878f3529296c9e8b9f23bc8d4902981915
ronentgreat <ronenh53@gmail.com>
2022-08-04 11:13:40 +0000

    .

--- quizzler-app/ui.py
+++ quizzler-app/ui.py
@@ -1,24 +1,29 @@
 from tkinter import *
 THEME_COLOR = "#375362"
 
+
 class QuizInterface:
     def __init__(self):
         self.window = Tk()
-        self.window.title("Quizzler")
+        self.window.title("Quizz Me")
         self.window.config(padx=30, pady=20, bg=THEME_COLOR)
         self.canvas = Canvas(width=300, height=250, bg='white')
-        self.question_body = self.canvas.create_text(150, 125, text="Some annoying text here because I'm tired.",
+        self.question_body = self.canvas.create_text(150, 125, text="Some Question Text",
                                                      fill='black', font=("Arial", 20, "italic"))
-        self.canvas.grid(column=0, row=0, columnspan=2)
+        self.canvas.grid(column=0, row=1, columnspan=2, pady=50)
+
+        # Score
+        self.score = Label(text='Score: 0', fg=THEME_COLOR)
+        self.score.grid(column=1, row=0)
 
         # False Button
-        self.false_img = PhotoImage(file='./images/false.png')
-        self.false_button = Button(image=self.false_img)
+        false_img = PhotoImage(file='./images/false.png')
+        self.false_button = Button(image=false_img, highlightthickness=0)
         self.false_button.grid(column=1, row=2)
 
         # True Button
-        self.true_img = PhotoImage(file='./images/true.png')
-        self.true_button = Button(image=self.true_img)
+        true_img = PhotoImage(file='./images/true.png')
+        self.true_button = Button(image=true_img, highlightthickness=0)
         self.true_button.grid(column=0, row=2)
 
         self.window.mainloop()
