c6f0ed6e58a4d272519118ea3659b07ca167b806
ronentgreat <ronenh53@gmail.com>
2022-08-04 22:18:57 +0000

    feature: question displays on window

--- quizzler-app/ui.py
+++ quizzler-app/ui.py
@@ -1,14 +1,19 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 THEME_COLOR = "#375362"
 
 
 class QuizInterface:
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
         self.window = Tk()
         self.window.title("Quizz Me")
         self.window.config(padx=30, pady=20, bg=THEME_COLOR)
         self.canvas = Canvas(width=300, height=250, bg='white')
-        self.question_body = self.canvas.create_text(150, 125, text="Some Question Text",
+        self.question_body = self.canvas.create_text(150,
+                                                     125,
+                                                     width=280,
+                                                     text="Some Question Text",
                                                      fill='black', font=("Arial", 20, "italic"))
         self.canvas.grid(column=0, row=1, columnspan=2, pady=50)
 
@@ -26,4 +31,10 @@ class QuizInterface:
         self.true_button = Button(image=true_img, highlightthickness=0)
         self.true_button.grid(column=0, row=2)
 
+        self.get_next_question()
+
         self.window.mainloop()
+
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_body, text=q_text)
