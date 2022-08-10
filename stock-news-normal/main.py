5b0ac12e65c51faaef3b9ee53572fdbe41ba49f9
ronentgreat <ronenh53@gmail.com>
2022-08-10 23:15:51 +0000

    done

--- stock-news-normal/main.py
+++ stock-news-normal/main.py
@@ -1,6 +1,7 @@
 import requests
 from dotenv import load_dotenv
 import os
+from twilio.rest import Client
 
 load_dotenv()
 api_key = os.getenv('api_key')
@@ -10,6 +11,9 @@ COMPANY_NAME = "Tesla Inc"
 
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
+
+account_sid = os.getenv('account_sid')
+auth_token = os.getenv('auth_token')
 
 
 stock_parameters = {
@@ -39,25 +43,35 @@ day_before_yesterday_data = data_list[1]
 day_before_yesterday_closing_price = day_before_yesterday_data['4. close']
 print(day_before_yesterday_closing_price)
 
-difference_closing_price = abs(float(yesterday_closing_price) - float(day_before_yesterday_closing_price))
-print(difference_closing_price)
+difference_closing_price = float(yesterday_closing_price) - float(day_before_yesterday_closing_price)
+up_down = None
+if difference_closing_price > 0:
+    up_down = "💹"
+else:
+    up_down = "📉"
+
 
 
 percentage = (difference_closing_price/float(yesterday_closing_price)) * 100
 print(percentage)
 
-if percentage > .5:
+if abs(percentage) > .5:
     news_response = requests.get(url=NEWS_ENDPOINT, params=news_parameters)
     news_response.raise_for_status()
     news_data = news_response.json()['articles']
     articles = news_data[:3]
-
-    formatted_articles = [f"Headline: {article['title']}, \nBrief: {article['description']}" for article in articles]
+    formatted_articles = [f"{STOCK_NAME}: {up_down} {percentage}% \nHeadline: {article['title']}, \nBrief: {article['description']}" for article in articles]
     print(formatted_articles)
 
+    client = Client(account_sid, auth_token)
+    for msg in formatted_articles:
+        message = client.messages.create(
+            body=msg,
+            from_='+15086918567',
+            to='+233543677965'
+        )
 
-
-#TODO 9. - Send each article as a separate message via Twilio. 
+#TODO 9. - Send each article as a separate message via Twilio.
 
 
 
