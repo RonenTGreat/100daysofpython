9fa2387c11bce18766e601e73b83fb54a5eb85dd
ronentgreat <ronenh53@gmail.com>
2022-08-14 23:31:00 +0000

    created graph

--- habit tracker/main.py
+++ habit tracker/main.py
@@ -1,13 +1,31 @@
 import requests
 
+USERNAME = "ronenthegreat"
+TOKEN = "eihidunijdoiidnife09udy"
 pixela_endpoint = "https://pixe.la/v1/users"
 
 user_params = {
-    "token": "eihidunijdoiidnife09udy",
-    "username": "ronenthegreat",
+    "token": TOKEN,
+    "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes"
 }
 
-response = requests.post(url=pixela_endpoint, json=user_params)
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+graph_config = {
+    "id": "graph1",
+    "name": "Coding Graph",
+    "unit": "Hr",
+    "type": "float",
+    "color": "momiji"
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
 print(response.text)
