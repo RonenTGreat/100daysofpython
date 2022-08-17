374f32a623833256750d9c751a7b72902d3975b1
ronentgreat <ronenh53@gmail.com>
2022-08-17 22:28:37 +0000

    done

--- Workout Tracking/main.py
+++ Workout Tracking/main.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import datetime
 from dotenv import load_dotenv
 import os
 
@@ -9,8 +10,9 @@ WEIGHT = 73.5
 GENDER = "male"
 AGE = 23
 
-
 nutritionix_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+sheet_endpoint = "https://api.sheety.co/bacad1c9188a01ac0462d5ad836c9089/myWorkouts/workouts"
+
 app_id = os.getenv("APP_ID")
 app_key = os.getenv("API_KEY")
 
@@ -29,5 +31,22 @@ headers = {
     "x-app-key": app_key,
 }
 
+today = datetime.now()
+
+
 response = requests.post(url=nutritionix_endpoint, json=post_query, headers=headers)
-print(response.json())
+exercise_data = response.json()
+
+for exercise in exercise_data["exercises"]:
+    sheet_input = {
+        "workout": {
+            "date": today.strftime("%d/%m/%Y"),
+            "time": today.strftime("%X"),
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"],
+        }
+    }
+
+    response_exercise = requests.post(url=sheet_endpoint, json=sheet_input, auth=("ronen", "bnVsbDpudWxs"))
+    print(response_exercise.text)
