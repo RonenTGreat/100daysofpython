76b53212476f86ee38fc8dbf9149ff4387c146b7
ronentgreat <ronenh53@gmail.com>
2022-08-20 22:33:41 +0000

    introducion done

--- bs4/main.py
+++ bs4/main.py
@@ -7,4 +7,21 @@ soup = BeautifulSoup(contents, 'html.parser')
 # print(soup.title)
 # print(soup.title.string)
 # print(soup.prettify())
-print(soup.a)
+# print(soup.a)
+
+all_anchor_tags = soup.findAll(name="a")
+# print(all_anchor_tags)
+for tag in all_anchor_tags:
+    # print(tag.string)
+    # print(tag.getText())
+    # print(tag.get('href'))
+    pass
+
+heading = soup.find(name='h1', id='name')
+# print(heading)
+
+section_heading = soup.find(name='h3', class_='heading')
+print(section_heading.getText())
+
+company_url = soup.select_one(selector='p a')
+print(company_url)
