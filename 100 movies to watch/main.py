79c6576b24fe5f5a7282e58cd5d6d0e6cbc1c622
ronentgreat <ronenh53@gmail.com>
2022-08-21 22:16:57 +0000

    refractor: added list comprehension

--- 100 movies to watch/main.py
+++ 100 movies to watch/main.py
@@ -9,12 +9,10 @@ movie_page = response.text
 soup = BeautifulSoup(movie_page, "html.parser")
 movies_list = []
 movies = soup.find_all(name='h3', class_='title')
-for movie in movies[::-1]:
-    text = movie.getText()
-    movies_list.append(text)
+movies_list = [text.getText() for text in movies[::-1]]
 with open('movies.txt', 'a', encoding="utf8") as file:
     for movie_list in movies_list:
-        file.write(movie_list + "\n")
+        file.write(f"{movie_list}\n")
 
 
 
