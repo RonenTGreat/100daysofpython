860ab7b68e4e39085e8e43ab57df388c8ff6bd28
ronentgreat <ronenh53@gmail.com>
2022-08-21 21:26:01 +0000

    done

--- 100 movies to watch/main.py
+++ 100 movies to watch/main.py
@@ -4,5 +4,17 @@ from bs4 import BeautifulSoup
 URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
 
 # Write your code below this line 👇
+response = requests.get(url=URL)
+movie_page = response.text
+soup = BeautifulSoup(movie_page, "html.parser")
+movies_list = []
+movies = soup.find_all(name='h3', class_='title')
+for movie in movies[::-1]:
+    text = movie.getText()
+    movies_list.append(text)
+with open('movies.txt', 'a', encoding="utf8") as file:
+    for movie_list in movies_list:
+        file.write(movie_list + "\n")
+
 
 
