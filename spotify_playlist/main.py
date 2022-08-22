10a4d229b9982a41bab61e531d495f2a2599abcb
ronentgreat <ronenh53@gmail.com>
2022-08-22 19:59:34 +0000

    step 1

---
+++ spotify_playlist/main.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+
+year = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
+
+URL = f"https://www.billboard.com/charts/hot-100/{year}"
+response = requests.get(url=URL)
+playlist_page = response.text
+soup = BeautifulSoup(playlist_page, "html.parser")
+songs = soup.select("li ul li h3")
+song_list = [song.getText().replace("\n", "").replace("\t", "") for song in songs]
