fa96664c7b46255a63e6c508fd5cc0335b870592
ronentgreat <ronenh53@gmail.com>
2022-08-22 19:59:20 +0000

    .

--- 100 movies to watch/main.py
+++ 100 movies to watch/main.py
@@ -3,7 +3,6 @@ from bs4 import BeautifulSoup
 
 URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
 
-# Write your code below this line 👇
 response = requests.get(url=URL)
 movie_page = response.text
 soup = BeautifulSoup(movie_page, "html.parser")
