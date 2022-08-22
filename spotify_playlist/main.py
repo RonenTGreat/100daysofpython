acd52840c9c4c35866b2981696459d924daf38d7
ronentgreat <ronenh53@gmail.com>
2022-08-22 20:16:34 +0000

    added keys

--- spotify_playlist/main.py
+++ spotify_playlist/main.py
@@ -1,5 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+client_id = os.getenv('CLIENT_ID')
+client_secret = os.getenv('CLIENT_SECRET')
 
 year = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
 
