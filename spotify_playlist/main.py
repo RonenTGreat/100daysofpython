5d50a6bddc02938605197f53b5d9235d76a9d9f9
ronentgreat <ronenh53@gmail.com>
2022-08-23 22:34:48 +0000

    done

--- spotify_playlist/main.py
+++ spotify_playlist/main.py
@@ -11,12 +11,15 @@ client_secret = os.getenv('CLIENT_SECRET')
 
 date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
 
+# Scraps through billboard charts than creates a list of the titles.
 URL = f"https://www.billboard.com/charts/hot-100/{date}"
 response = requests.get(url=URL)
 playlist_page = response.text
 soup = BeautifulSoup(playlist_page, "html.parser")
 songs = soup.select("li ul li h3")
 song_list = [song.getText().replace("\n", "").replace("\t", "") for song in songs]
+
+# Sets up Spotify Authentication
 song_uris = []
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
     scope="playlist-modify-private",
@@ -27,7 +30,7 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
     cache_path="token.txt"
 ))
 
-
+# Searches the songs from the list created then passes it to Spotify then added it to a list
 user_id = sp.current_user()["id"]
 year = date.split("-")[0]
 
@@ -40,4 +43,7 @@ for song in song_list:
     except IndexError:
         print(f"{song} doesn't exist in Spotify. Skipped.")
 
+# Creates Playlist and adds it to your library
+playlist = sp.user_playlist_create(user=user_id, name=f"{date} Billboard 100", public=False)
+sp.playlist_add_items(playlist_id=playlist["id"], items=song_uris)
 
