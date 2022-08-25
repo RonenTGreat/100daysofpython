286e746b1314462e1740028bc8f39de1bfe31eb0
ronentgreat <ronenh53@gmail.com>
2022-08-25 23:14:01 +0000

    first step done

--- Cookie_Clicker/main.py
+++ Cookie_Clicker/main.py
@@ -3,4 +3,6 @@ from selenium.webdriver.common.by import By
 chrome_driver_path = "C:\Development\chromedriver"
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
 
-driver.get("https://orteil.dashnet.org/cookieclicker/")
+driver.get("http://orteil.dashnet.org/experiments/cookie/")
+cookie_button = driver.find_element(By.CSS_SELECTOR, "#cookie")
+cookie_button.click()
