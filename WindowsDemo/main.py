30d54178416d9609484b16abe1d59afc75621aa8
ronentgreat <ronenh53@gmail.com>
2022-08-25 21:07:43 +0000

    challenge 1

--- WindowsDemo/main.py
+++ WindowsDemo/main.py
@@ -13,6 +13,15 @@ driver.get("https://www.python.org")
 # documentation_link = driver.find_element(By.CSS_SELECTOR, ".documentation-widget a")
 # print(documentation_link.text)
 
+event_times = driver.find_elements(By.CSS_SELECTOR, '.event-widget time')
+event_names = driver.find_elements(By.CSS_SELECTOR, '.event-widget li a')
+events = {}
+for n in range(len(event_times)):
+    events[n] = {
+        "time": event_times[n].text,
+        "name": event_names[n].text
+    }
+print(events)
 
 driver.quit()
 # driver.close()
