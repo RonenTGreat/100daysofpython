f97b4ef06ccb68fb5701faaae400972fcdde8086
ronentgreat <ronenh53@gmail.com>
2022-08-27 23:05:23 +0000

    .

--- Cookie_Clicker/main.py
+++ Cookie_Clicker/main.py
@@ -4,7 +4,6 @@ import time
 
 chrome_driver_path = "C:\Development\chromedriver"
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
-
 driver.get("http://orteil.dashnet.org/experiments/cookie/")
 cookie = driver.find_element(By.ID, "cookie")
 products = driver.find_elements(By.CSS_SELECTOR, "#rightPanel b")[:-1]
