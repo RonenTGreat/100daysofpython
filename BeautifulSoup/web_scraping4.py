43f7c9805a4187aa813c3caaa4262caf3518600b
ronentgreat <ronenh53@gmail.com>
2022-09-01 23:53:54 +0000

    done

--- BeautifulSoup/web_scraping4.py
+++ BeautifulSoup/web_scraping4.py
@@ -10,12 +10,32 @@ doc = BeautifulSoup(page, "html.parser")
 page_text = doc.find(class_="list-tool-pagination-text").strong
 pages = int(str(page_text).split("/")[-2].split(">")[1][0])
 
+items_found = {}
+
 for page in range(1, pages + 1):
     url = f"https://www.newegg.ca/p/pl?d={search_term}&N=4131&page={pages}"
     page = requests.get(url).text
     doc = BeautifulSoup(page, "html.parser")
 
-    items = doc.find_all(text=re.compile(search_term))
-    for item in items:
-        print(item)
+    div = doc.find(class_="item-cells-wrap border-cells items-grid-view four-cells expulsion-one-cell")
+    items = div.find_all(text=re.compile(search_term))
 
+    for item in items:
+        parent = item .parent
+        if parent.name != "a":
+            continue
+        link = parent["href"]
+        next_parent = item.find_parent(class_="item-container")
+        try:
+            price = next_parent.find(class_="price-current").strong.string
+            items_found[item] = {"price": int(price.replace(",", "")), "link": link}
+        except:
+            pass
+
+sorted_items = sorted(items_found.items(), key=lambda x: x[1]['price'])
+
+for item in sorted_items:
+    print(item[0])
+    print(f"${item[1]['price']}")
+    print(item[1]['link'])
+    print("-------------------------------------")
