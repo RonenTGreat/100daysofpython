5ae10657f8fee5f8eada62ceddf59056ec58fc6c
ronentgreat <ronenh53@gmail.com>
2022-09-02 23:51:44 +0000

    .

--- python_selenium/main.py
+++ python_selenium/main.py
@@ -1,6 +1,11 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 
 chrome_driver_path = r"C:\Development\chromedriver"
 service = Service(chrome_driver_path)
 driver = webdriver.Chrome(service=service)
+driver.get("https://www.ronenhammond.me")
+driver.implicitly_wait(5)
+my_element = driver.find_element(By.CLASS_NAME, "cta-resume")
+my_element.click()
