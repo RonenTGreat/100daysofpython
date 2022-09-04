f3a327c41b121f5ec5895a847d902d85ea94b7b4
ronentgreat <ronenh53@gmail.com>
2022-09-04 23:59:54 +0000

    .

--- python_selenium/main.py
+++ python_selenium/main.py
@@ -1,5 +1,7 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 
 chrome_driver_path = r"C:\Development\chromedriver"
@@ -7,5 +9,10 @@ service = Service(chrome_driver_path)
 driver = webdriver.Chrome(service=service)
 driver.get("https://www.techwithtim.net/")
 driver.implicitly_wait(5)
-my_element = driver.find_element(By.CLASS_NAME, "cta-resume")
-my_element.click()
+
+search = driver.find_element(By.NAME, "s")
+search.send_keys("test")
+search.send_keys(Keys.RETURN)
+
+time.sleep(5)
+driver.quit()
