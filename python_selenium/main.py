29fabe42a2cdda187b297a5c8ed2830cea65feff
ronentgreat <ronenh53@gmail.com>
2022-09-05 00:31:17 +0000

    .

--- python_selenium/main.py
+++ python_selenium/main.py
@@ -1,8 +1,9 @@
-import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 chrome_driver_path = r"C:\Development\chromedriver"
 service = Service(chrome_driver_path)
@@ -14,5 +15,13 @@ search = driver.find_element(By.NAME, "s")
 search.send_keys("test")
 search.send_keys(Keys.RETURN)
 
-time.sleep(5)
-driver.quit()
+try:
+    main = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "main"))
+    )
+    articles = main.find_elements(By.TAG_NAME, "article")
+    for article in articles:
+        header = article.find_element(By.CLASS_NAME, "entry-summary")
+        print(header.text)
+finally:
+    driver.quit()
