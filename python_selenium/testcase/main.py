c163464bf10473f83681534e0b1b3044c34a67cc
ronentgreat <ronenh53@gmail.com>
2022-09-09 21:46:55 +0000

    created test case

--- python_selenium/testcase/main.py
+++ python_selenium/testcase/main.py
@@ -11,3 +11,13 @@ class PythonOrgSearch(unittest.TestCase):
         service = Service(chrome_driver_path)
         self.driver = webdriver.Chrome(service=service)
         self.driver.get("http://www.python.org")
+
+    def test_example(self):
+        print("Test")
+        assert True
+
+    def not_a_test(self):
+        print("this won't print")
+
+    def tearDown(self) -> None:
+        self.driver.close()
