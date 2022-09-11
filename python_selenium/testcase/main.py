528628a40182ef92d5e3d83050a4a93c3f8a2ab1
ronentgreat <ronenh53@gmail.com>
2022-09-11 21:33:07 +0000

    completed

--- python_selenium/testcase/main.py
+++ python_selenium/testcase/main.py
@@ -12,9 +12,13 @@ class PythonOrgSearch(unittest.TestCase):
         self.driver = webdriver.Chrome(service=service)
         self.driver.get("http://www.python.org")
 
-    def test_title(self):
-       mainPage = page.MainPage()
-       assert mainPage.is_title_matches()
+    def test_search_python(self):
+        mainPage = page.MainPage(self.driver)
+        assert mainPage.is_title_matches()
+        mainPage.search_text_element = "pycon"
+        mainPage.click_go_button()
+        search_result_page = page.SearchResultPage(self.driver)
+        assert search_result_page.is_result_found()
 
     def tearDown(self) -> None:
         self.driver.close()
--- python_selenium/testcase/page.py
+++ python_selenium/testcase/page.py
@@ -1,12 +1,20 @@
 from locator import *
 from element import BasePageElement
 
+
+class SearchTextElement(BasePageElement):
+    locator = "q"
+
+
 class BasePage(object):
     def __init__(self, driver):
         self.driver = driver
 
 
 class MainPage(BasePage):
+
+    search_text_element = SearchTextElement()
+
     def is_title_matches(self):
         return "Python" in self.driver.title
 
