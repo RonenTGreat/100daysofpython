e6fa68ac801469b4ff7e84284e66cb92526d07e7
ronentgreat <ronenh53@gmail.com>
2022-09-12 21:33:11 +0000

    .

--- hello_flask/hello.py
+++ hello_flask/hello.py
@@ -5,4 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return '<p style="color:red;font-weight:bold;">Hello, World!</p>'
+    return '<p style="color:blue;font-weight:bold;">Hello, World!</p>'
+
+if __name__ == "__main__":
+    app.run()
