3b518d1d91f9d716e8ca09c184e2e9741ed7ae56
ronentgreat <ronenh53@gmail.com>
2022-09-13 22:35:52 +0000

    added a route resume

--- hello_flask/hello.py
+++ hello_flask/hello.py
@@ -7,5 +7,26 @@ app = Flask(__name__)
 def hello_world():
     return '<p style="color:blue;font-weight:bold;">Hello, World!</p>'
 
+@app.route("/resume")
+def resume():
+    return (
+        "<div>"
+        
+        '<h1 style="text-align:center;">Ronen Nii Ayi Hammond</h2>'
+        '<h4 style="text-align:center;">Accra-Ghana, Phone: +233543677965</h4>'
+        '<h4 style="text-align:center;">ronenhammond@gmail.com</h4>'
+        "<h2>Education</h2>"
+        "<hr />"
+        '<h3>University of Ghana</h3>'
+        '<p>Programme: Bachelor of Science, Information Technology <span style="text-align:right; margin-left: 30rem">'
+        'Expected September 2024</span></p>'
+        '<p>Academic Standing: Second Upper</p>'
+        '<p>Relevant Courses: Introduction to Computer Science, Programming Fundamentals, Programming I, Introduction t'
+        'oEconomics, Mathematics for IT Professionals, Date Structures and Algorithms, Office Productivity Tools, Software Engineering, Multimedia and Web Development</p>'
+        
+
+        "</div>"
+    )
+
 if __name__ == "__main__":
     app.run()
