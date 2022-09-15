43ed28b4c14279334a1713e31b16622775256783
ronentgreat <ronenh53@gmail.com>
2022-09-15 22:39:29 +0000

    decorators

--- hello_flask/hello.py
+++ hello_flask/hello.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 def hello_world():
     return '<p style="color:blue;font-weight:bold;">Hello, World!</p>'
 
+
 @app.route("/resume")
 def resume():
     return (
@@ -22,13 +23,42 @@ def resume():
         'Expected September 2024</span></p>'
         '<p>Academic Standing: Second Upper</p>'
         '<p>Relevant Courses: Introduction to Computer Science, Programming Fundamentals, Programming I, Introduction t'
-        'oEconomics, Mathematics for IT Professionals, Date Structures and Algorithms, Office Productivity Tools, Software Engineering, Multimedia and Web Development</p>'
+        'oEconomics, Mathematics for IT Professionals, Date Structures and Algorithms, Office Productivity Tools, '
+        'Software Engineering, Multimedia and Web Development</p>'
         "</div>"
     )
+
 
 @app.route("/<name>")
 def greet(name):
     return f"Hello {name}"
+
+
+def make_bold(function):
+    def wrapper_function():
+        return f"<b>{function()}</b>"
+    return wrapper_function
+
+
+def make_emphasis(function):
+    def wrapper_function():
+        return f"<em>{function()}</em>"
+    return wrapper_function
+
+
+def make_underlined(function):
+    def wrapper_function():
+        return f"<u>{function()}</u>"
+    return wrapper_function
+
+
+@app.route("/bye")
+@make_bold
+@make_emphasis
+@make_underlined
+def bye():
+    return "Bye"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
