8170c5e3f45c33fb479f12f7f0a34af33d65dd29
ronentgreat <ronenh53@gmail.com>
2022-09-16 23:38:38 +0000

    added gif

--- higher-lower/server.py
+++ higher-lower/server.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Guess a number between 0 and 9</h1>"
+    return '<h1>Guess a number between 0 and 9</h1><img src="https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif" />'
 
 
 if __name__ == "__main__":
