17ee5f16556525abb95ec71a7c955a857f3a6441
ronentgreat <ronenh53@gmail.com>
2022-09-18 21:59:46 +0000

    added statics files

--- my-personal-site/server.py
+++ my-personal-site/server.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return "Hello World"
+    return render_template("ronen.html")
 
 
 if __name__ == "__main__":
