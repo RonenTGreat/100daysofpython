c36c1e16efe046e117726053c2d04a8b837f0de2
ronentgreat <ronenh53@gmail.com>
2022-09-19 20:48:15 +0000

    challenge begin

--- day-57-start/main.py
+++ day-57-start/main.py
@@ -1,11 +1,22 @@
 from flask import Flask, render_template
+import random
+from datetime import datetime
 
 app = Flask(__name__)
+
+current_year = datetime.now().year
+
 
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    random_number = random.randint(1, 10)
+    return render_template("index.html", num=random_number, year=current_year)
+
+
+@app.route("/guess/<string:name>")
+def guess(name):
+    return f"{name}"
 
 
 if __name__ == "__main__":
