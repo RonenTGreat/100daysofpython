c11c4dbbac6673e2c9711acc64932f1d8d668c82
ronentgreat <ronenh53@gmail.com>
2022-09-19 21:18:50 +0000

    added age

--- day-57-start/main.py
+++ day-57-start/main.py
@@ -18,8 +18,11 @@ def home():
 @app.route("/guess/<string:name>")
 def guess(name):
     response_gender = requests.get(url=f"https://api.genderize.io/?name={name}")
+    response_age = requests.get(url=f"https://api.agify.io/?name={name}")
     gender = response_gender.json()['gender']
-    return render_template("guess.html", name=name, gender=gender)
+    age = response_age.json()['age']
+
+    return render_template("guess.html", name=name, gender=gender, age=age)
 
 
 if __name__ == "__main__":
