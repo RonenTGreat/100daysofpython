5732e335c006782d57bca47b85cb5da4341e4053
ronentgreat <ronenh53@gmail.com>
2022-09-21 21:44:26 +0000

    completed

--- blog-templating/main.py
+++ blog-templating/main.py
@@ -1,11 +1,21 @@
 from flask import Flask, render_template
-
+from post import Post
 
 app = Flask(__name__)
 
+post = Post()
+all_blogs = post.all_blogs
+
+print(all_blogs)
+
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("index.html", all_blogs=all_blogs)
+
+@app.route('/post/<int:num>')
+def blogs(num):
+    return render_template("post.html", all_blogs=all_blogs, num=num)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- blog-templating/post.py
+++ blog-templating/post.py
@@ -1,2 +1,14 @@
+import requests
+
+url = "https://api.npoint.io/c790b4d5cab58020d391"
+
+
 class Post:
-    pass
+    def __init__(self):
+        self.all_blogs = []
+        self.get_blog()
+
+    def get_blog(self):
+        response = requests.get(url)
+        self.all_blogs = response.json()
+        return self.all_blogs
